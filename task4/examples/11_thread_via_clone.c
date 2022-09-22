2ae79c35a1a9934b9c2787f6cf409535ded040b1
nefanov <efanov@phystech.edu>
2022-09-22 21:48:20 +0300

    Update 11_thread_via_clone.c

--- task4/examples/11_thread_via_clone.c
+++ task4/examples/11_thread_via_clone.c
@@ -9,6 +9,32 @@
 
 #define TIMEOUT 5
 
+/* *********** used flags description from MANual pages (man 2 clone) *************
+If CLONE_VM is set, the calling process and the child
+              process run in the same memory space.  In particular,
+              memory writes performed by the calling process or by the
+              child process are also visible in the other process.
+              Moreover, any memory mapping or unmapping performed with
+              mmap(2) or munmap(2) by the child or calling process also
+              affects the other process.
+  CLONE_SIGHAND (since Linux 2.0)
+              If CLONE_SIGHAND is set, the calling process and the child
+              process share the same table of signal handlers.  If the
+              calling process or child process calls sigaction(2) to
+              change the behavior associated with a signal, the behavior
+              is changed in the other process as well.  However, the
+              calling process and child processes still have distinct
+              signal masks and sets of pending signals.  So, one of them
+              may block or unblock signals using sigprocmask(2) without
+              affecting the other process.
+  CLONE_THREAD (since Linux 2.4.0)
+              If CLONE_THREAD is set, the child is placed in the same
+              thread group as the calling process.  To make the
+              remainder of the discussion of CLONE_THREAD more readable,
+              the term "thread" is used to refer to the processes within
+              a thread group.
+*/
+
 static int child_func(void *arg) {
     printf("Child thread PID=%d TID=%d\n", getpid(),gettid());
     sleep(TIMEOUT);
