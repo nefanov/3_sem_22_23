876342690ccbed92d1f96dd3e73e5599c5eca2ee
NA <nik21032004@gmail.com>
2022-11-07 03:21:20 +0300

    add good integrator

--- task4/my_code/code/Src/integer.c
+++ task4/my_code/code/Src/integer.c
@@ -13,7 +13,7 @@ const size_t DPS = 10000000;
 
 double f (double x);
 
-double rand_d(double min, double max);
+double rand_d(double min, double max, unsigned int* seed);
 
 double one_stream_F(double a, double b, double y_min, double y_max, double f(double x));
 
@@ -23,8 +23,6 @@ typedef struct tread_data
     double b; 
     double y_min; 
     double y_max;
-    size_t tread_num;
-    size_t my_tread;
 } data;
 
 
@@ -58,38 +56,13 @@ double f (double x)
 }
 
 
-double rand_d(double min, double max)
+inline double rand_d(double min, double max, unsigned int* seed)
 {
    
     double range = (double)RAND_MAX/(max - min);
-    double offset = rand()/range;
+    double offset = rand_r(seed)/range;
 
     return min + offset; 
-}
-
-double one_stream_F (double a, double b, double y_min, double y_max, double f(double x))
-{
-    assert(b > a);
-    assert(y_max > y_min);
-    
-    double S_sell = (y_max - y_min)*(b - a);
-
-    size_t dot_num = S_sell*(double)DPS;
-    size_t dot_ander = 0;
-
-    size_t itter = 0;
-
-    for(itter = 0; itter < dot_num; itter++)
-    {
-        double rand_x = rand_d(a, b);
-        double rand_y = rand_d(y_min, y_max);
-
-        if(rand_y < (*f)(rand_x)) dot_ander++;
-    }
-    
-    //printf("a:%lf, b:%lf, y_min:%lf, y:max:%lf, ans:%lf, dot num:%lu \n", a, b, y_min, y_max, (S_sell*dot_ander)/dot_num, dot_num);
-
-    return ((S_sell*dot_ander)/dot_num);
 }
 
 void F_init()
@@ -107,11 +80,30 @@ void* sell_F (void* arg)
     double b = args.b;
     double y_min = args.y_min;
     double y_max = args.y_max;
-    size_t tread_num = args.tread_num;
 
-    size_t my_tread = args.my_tread;
+    assert(b > a);
+    assert(y_max > y_min);
+    
+    double S_sell = (y_max - y_min)*(b - a);
 
-    add_to_ineger(one_stream_F(a + ((b - a) * my_tread)/tread_num, a + ((b - a) * (my_tread + 1))/tread_num, y_min, y_max, f));
+    unsigned int seed = time(NULL);
+
+    size_t dot_num = S_sell*(double)DPS;
+    size_t dot_ander = 0;
+
+    size_t itter = 0;
+
+    for(itter = 0; itter < dot_num; itter++)
+    {
+        double rand_x = rand_d(a, b, &seed);
+        double rand_y = rand_d(y_min, y_max, &seed);
+
+        if(rand_y < (*f)(rand_x)) dot_ander++;
+    }
+    
+    //printf("a:%lf, b:%lf, y_min:%lf, y:max:%lf, ans:%lf, dot num:%lu \n", a, b, y_min, y_max, (S_sell*dot_ander)/dot_num, dot_num);
+
+    add_to_ineger((S_sell*dot_ander)/dot_num);
     
 }
 
@@ -126,12 +118,10 @@ double F (double a, double b, double y_min, double y_max, size_t treads_num)
 
     for(tread_itter = 0; tread_itter < treads_num; tread_itter ++)
     {
-        (args_data + tread_itter)->a = a;
-        (args_data + tread_itter)->b = b;
+        (args_data + tread_itter)->a = a + ((b-a)*tread_itter)/treads_num;
+        (args_data + tread_itter)->b = a + ((b-a)*(tread_itter + 1))/treads_num;
         (args_data + tread_itter)->y_max = y_max;
         (args_data + tread_itter)->y_min = y_min;
-        (args_data + tread_itter)->my_tread = tread_itter;
-        (args_data + tread_itter)->tread_num = treads_num;
         
         pthread_create(&(My_treads[tread_itter]), NULL, sell_F, (args_data + tread_itter));
     }
