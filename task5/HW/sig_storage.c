e10b5670792ccb3328338cc8f1dd835c7ee3dbb4
fatalick <fatalick01@gmail.com>
2022-12-05 18:42:48 +0300

    rewrited task5 to working variant

--- task5/HW/sig_storage.c
+++ task5/HW/sig_storage.c
@@ -1,172 +1,203 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <sys/mman.h>
-#include <sys/signal.h>
+#include <fcntl.h>
 #include <sys/shm.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <errno.h>
 
-#define NAME "AWESOME"
-#define SIZE 4096
-
-static pid_t pid;
-static int out_char = 0;
-static int counter = 128;
-
-// SIG_CHLD
-void exit_child(int signo) 
+enum MY_SIG
 {
-  exit(EXIT_SUCCESS);
+    WRITE_SIG = 45,
+    READ_SIG  = 46,
+    END_SIG   = 2
+};
+
+enum BOOL_STATUS
+{
+    WRITE_BOOL  = 1,
+    READ_BOOL   = 2,
+    END_WR_BOOL = 3,
+    END_RD_BOOL = 4
+};
+
+short parent_flag     = 1;
+short child_flag      = 0;
+short parent_flag_end = 0;
+short child_flag_end  = 0;
+
+const char* name = "AWESOMESLAYER";
+
+#define SIZE 20480
+
+void parent_sigHandler(int signo, siginfo_t* siginf, void* context)
+{
+    parent_flag = WRITE_BOOL;
 }
 
-// SIG_ALRM
-void exit_parent(int signo) 
+void parent_terminationHandler(int signo, siginfo_t* siginf, void* context)
 {
-  exit(EXIT_SUCCESS);
+    parent_flag_end = END_WR_BOOL;
 }
 
-// nothing
-void nothing(int signo){}
-
-// SIG_USR1
-void got_one(int signo) 
+void child_sigHandler(int signo, siginfo_t* siginf, void* context)
 {
-  out_char += counter;
-  counter /= 2;
-  kill(pid, SIGUSR1);
+    child_flag = READ_BOOL;
+    FILE* output = fopen("output.txt", "a");
+
+    fprintf(output, "%s\n", (char*)siginf->si_value.sival_ptr );
+    fclose(output);
+
+    FILE* log = fopen("log.txt", "a" );
+    fprintf(log, "message was read\n" );
+    fclose(log);
+
+    return;
 }
 
-// SIG_USR2
-void got_zero(int signo) 
+void child_terminationHandler(int signo, siginfo_t* siginf, void* context)
 {
-  counter /= 2;
-  kill(pid, SIGUSR1);
+    child_flag_end = END_RD_BOOL;   
 }
 
-int main(int argc, char *argv[]) 
+
+int main(int argc, const char* argv[])
 {
-    if (argc != 2) 
-    {
-        printf("Use: %s [destination]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    int shm_fd = shm_open( NAME, O_CREAT | O_RDWR, 0666 );
-    void* ptr;
-
+    int shm_fd = -1;
+ 
+    void* ptr = NULL;
+ 
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+ 
     ftruncate(shm_fd, SIZE);
+ 
     ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    if ( ptr == MAP_FAILED ) 
+    if(ptr == MAP_FAILED)
     {
         perror("mmap");
-        return -1;
-    } 
-
-    // Получаем pid родителя
-    pid_t parent_pid = getpid();
-    sigset_t set;
-
-    // SIG_CHLD -> exit
-    struct sigaction act_exit;
-    memset(&act_exit, 0, sizeof(act_exit));
-    act_exit.sa_handler = exit_child;
-    sigfillset(&act_exit.sa_mask);
-    sigaction(SIGCHLD, &act_exit, NULL);
-
-    // SIG_USR1 -> got_one()
-    struct sigaction act_one;
-    memset(&act_one, 0, sizeof(act_one));
-    act_one.sa_handler = got_one;
-    sigfillset(&act_one.sa_mask);
-    sigaction(SIGUSR1, &act_one, NULL);
-
-    // SIG_USR2 -> got_zero()
-    struct sigaction act_zero;
-    memset(&act_zero, 0, sizeof(act_zero));
-    act_zero.sa_handler = got_zero;
-    sigfillset(&act_zero.sa_mask);
-    sigaction(SIGUSR2, &act_zero, NULL);
-
-    // Добавляем блокировки
-    sigaddset(&set, SIGUSR1);
-    sigaddset(&set, SIGUSR2);
-    sigaddset(&set, SIGCHLD);
-    sigprocmask(SIG_BLOCK, &set, NULL);
-    sigemptyset(&set);
-
-    pid = fork();
-
-    // CHILD (sender)
-    if (pid == 0) 
-    {
-        int fd = 0;
-        char c = 0;
-        sigemptyset(&set);
-
-        // SIG_USR1 -> nothing()
-        struct sigaction act_empty;
-        memset(&act_empty, 0, sizeof(act_empty));
-        act_empty.sa_handler = nothing;
-        sigfillset(&act_empty.sa_mask);
-        sigaction(SIGUSR1, &act_empty, NULL);
-
-        // SIG_ALRM -> exit_parent()
-        struct sigaction act_alarm;
-        memset(&act_alarm, 0, sizeof(act_alarm));
-        act_alarm.sa_handler = exit_parent;
-        sigfillset(&act_alarm.sa_mask);
-        sigaction(SIGALRM, &act_alarm, NULL);
-
-        int i;
-        size_t size = 0;
-
-        while (read(fd, &c, 1) > 0)
-        {
-            // SIGALRM Будет получен если родитель не успеет ответить за секунду
-            alarm(1);
-
-            // Побитовые операции
-            //printf("Char sent: %c \n", c);
-            for ( i = 128; i >= 1; i /= 2)
-            {
-                if ( i & c )              // 1
-                    kill(parent_pid, SIGUSR1);
-                else                      // 0
-                    kill(parent_pid, SIGUSR2);
-                
-                // Ждём подтверждения от родителя
-                // приостанавливает процесс до получения сигнала
-                sigsuspend(&set);
-            }
-            size += strlen( (char*)(ptr + size));
-        }
-        // ввод кончился
-        exit(EXIT_SUCCESS);
+        exit(1);
     }
 
-    // Получаем пока ребёнок не умрёт
-    FILE *fout = fopen(argv[1], "w");
-    do 
+    pid_t parent_pid = getpid();
+    pid_t child_pid  = fork();
+
+    if (child_pid == -1)
     {
-        if(counter == 0) 
-        {
-            write(fileno(fout), &out_char, 1);
-            fflush(fout);
-            counter=128;
-            //printf("Char received: %c \n", out_char);
-            out_char = 0;
+        perror("fork:");
+        exit(1);
+    }
+
+    if (child_pid > 0) 
+    {
+        size_t size = 0;
+        
+        sigset_t mask;
+
+        sigemptyset(&mask);
+        sigaddset(&mask, WRITE_SIG);
+        sigaddset(&mask, END_SIG);
+
+        struct sigaction sa_write, sa_end;
+
+        sa_end.sa_sigaction   = parent_terminationHandler;
+        sa_end.sa_mask        = mask;
+        sa_end.sa_flags       = SA_SIGINFO;
+        sa_write.sa_sigaction = parent_sigHandler;
+        sa_write.sa_mask      = mask;
+        sa_write.sa_flags     = SA_SIGINFO;
+        
+        union sigval val;
+
+        sigaction(END_SIG,   &sa_end,   NULL);
+        sigaction(WRITE_SIG, &sa_write, NULL);
+
+        FILE* log = NULL;
+
+        while (1) {
+
+            if (parent_flag == WRITE_BOOL) {
+                
+                if ( scanf( "%[^\n]", (char*)(ptr + size) ) == 1 )
+                {    
+                    while( getchar() != '\n' );
+                    val.sival_ptr = ptr + size;
+                    parent_flag = 0;
+                    log = fopen("log.txt", "a" );
+                    fprintf(log, "MESSAGE WAS WRITTEN, MSG:\"%s\"\n", (char*)(ptr + size));
+                    fclose(log);
+                    size += strlen( (char*)(ptr + size) );
+                    if ( size >= SIZE ) size = 0;
+                    sigqueue( child_pid, READ_SIG, val);
+                }
+
+            }
+
+            if ( parent_flag_end == END_WR_BOOL ) 
+            {
+                log = fopen("log.txt", "a" );
+                fprintf( log, "parent work is done\n");
+                fclose( log );
+                child_flag_end  = END_RD_BOOL;
+                parent_flag_end = 0;
+                parent_flag     = 0;
+                int status = 0;
+                waitpid(child_pid, &status, 0);
+                kill(child_pid, END_SIG);
+                return 0;
+            }
         }
-        sigsuspend(&set); // Ждём сигнал от ребёнка
-    } while (1);
+    } 
+    else
+    {
+        close(0);
+        close(1);
+        close(2);
+        FILE* log = NULL;
+        
+        sigset_t mask;
+        sigemptyset( &mask );
+        sigaddset( &mask, END_SIG );
+        sigaddset( &mask, READ_SIG );
 
+        struct sigaction sa_end, sa_read;
+        
+        sa_end.sa_sigaction = child_terminationHandler;
+        sa_end.sa_mask = mask;
+        sa_end.sa_flags = SA_SIGINFO;
+        sa_read.sa_sigaction = child_sigHandler;
+        sa_read.sa_mask = mask;
+        sa_read.sa_flags = SA_SIGINFO;
+        
+        sigaction(END_SIG,  &sa_end,  NULL);
+        sigaction(READ_SIG, &sa_read, NULL);
 
-    shm_unlink( NAME );
-    munmap( ptr, SIZE );
-    kill( parent_pid, SIGCHLD );
-    exit(EXIT_SUCCESS);
+        while(1) 
+        {
+
+            if (child_flag == READ_BOOL) 
+            {
+                
+                child_flag = 0;
+                kill( parent_pid, WRITE_SIG);
+            }   
+
+            if ( child_flag_end == END_RD_BOOL ) 
+            {
+                
+                log = fopen("log.txt", "a" );
+                fprintf(log, "child work is done\n");
+                fclose(log);
+                shm_unlink(name);
+                munmap(ptr, SIZE);
+                kill(parent_pid, SIGCHLD);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
 }
